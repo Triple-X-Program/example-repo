23a07407043a9b50c4ccfaeebfcd791e1ae9abb2
Jerome Moses <87420016+P-EO@users.noreply.github.com>
2021-10-26 10:08:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ rm -rf example-repo
   git clone https://github.com/Triple-X-Program/example-repo || error "failed to clone try cheking your internet."
   
   # after:
+  echo "Recloning example-repo"
   git_clone https://github.com/Triple-X-Program/example-repo || exit 1
